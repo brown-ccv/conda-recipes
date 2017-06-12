7a7e36088bf46f136ae107c2578bb1c802e537e3
Mark Howison <mhowison@brown.edu>
2017-06-11 21:14:54 -0400

    r-forestfloor 1.11.1

--- vim/build.sh
+++ vim/build.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-./configure --prefix=$PREFIX    \
-            --without-x         \
-            --without-gnome     \
-            --without-tclsh     \
-            --without-local-dir \
-            --enable-gui=no     \
-            --enable-cscope     \
+export CFLAGS=-I${PREFIX}/include
+export LDFLAGS=-L${PREFIX}/lib
+
+./configure --prefix=${PREFIX}       \
+            --without-x              \
+            --without-gnome          \
+            --without-tclsh          \
+            --without-local-dir      \
+            --enable-gui=no          \
+            --enable-cscope          \
             --enable-pythoninterp=yes
 
 make
